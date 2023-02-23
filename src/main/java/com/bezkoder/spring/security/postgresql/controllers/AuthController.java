771d51da08cb41080fb8aec0af0ce20bbfa3a96e
Carlos Guzman <carlosaguzman@gmail.com>
2023-02-23 15:26:12 -0700

    - initial project - implemented signup/login API

---
+++ src/main/java/com/bezkoder/spring/security/postgresql/controllers/AuthController.java
@@ -0,0 +1,131 @@
+package com.bezkoder.spring.security.postgresql.controllers;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bezkoder.spring.security.postgresql.models.ERole;
+import com.bezkoder.spring.security.postgresql.models.Role;
+import com.bezkoder.spring.security.postgresql.models.User;
+import com.bezkoder.spring.security.postgresql.payload.request.LoginRequest;
+import com.bezkoder.spring.security.postgresql.payload.request.SignupRequest;
+import com.bezkoder.spring.security.postgresql.payload.response.JwtResponse;
+import com.bezkoder.spring.security.postgresql.payload.response.MessageResponse;
+import com.bezkoder.spring.security.postgresql.repository.RoleRepository;
+import com.bezkoder.spring.security.postgresql.repository.UserRepository;
+import com.bezkoder.spring.security.postgresql.security.jwt.JwtUtils;
+import com.bezkoder.spring.security.postgresql.security.services.UserDetailsImpl;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+	@Autowired
+	AuthenticationManager authenticationManager;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	RoleRepository roleRepository;
+
+	@Autowired
+	PasswordEncoder encoder;
+
+	@Autowired
+	JwtUtils jwtUtils;
+
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = jwtUtils.generateJwtToken(authentication);
+		
+		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();		
+		List<String> roles = userDetails.getAuthorities().stream()
+				.map(item -> item.getAuthority())
+				.collect(Collectors.toList());
+
+		return ResponseEntity.ok(new JwtResponse(jwt, 
+												 userDetails.getId(), 
+												 userDetails.getUsername(),
+												 userDetails.getEmail(), 
+												 roles));
+	}
+
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+
+		boolean isExistingEmail = userRepository.existsByEmail(signUpRequest.getEmail());
+		if (isExistingEmail) {
+			Optional<User> existingUser = userRepository.findByEmail(signUpRequest.getEmail());
+			if (existingUser.isPresent()) {
+				User user = existingUser.get();
+				Set<Role> roles  = user.getRoles();
+				final boolean[] isAdmin = {false};
+				roles.forEach(role -> {
+					if (role.getName().equals(ERole.ROLE_ADMIN)) {
+						isAdmin[0] = true;
+					}
+				});
+				if (isAdmin[0]) {
+					return ResponseEntity
+							.badRequest()
+							.body(new MessageResponse("Error: Email is already in use!"));
+
+				} else {
+					User newUser = new User( signUpRequest.getEmail(),
+							signUpRequest.getEmail(),
+							encoder.encode(signUpRequest.getPassword()));
+
+					Set<Role> userRoles = new HashSet<>();
+					Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					userRoles.add(userRole);
+					newUser.setRoles(userRoles);
+
+					userRepository.save(newUser);
+					return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+				}
+			}
+		} else {
+			User user = new User( signUpRequest.getEmail(),
+					signUpRequest.getEmail(),
+					encoder.encode(signUpRequest.getPassword()));
+
+			Set<Role> roles = new HashSet<>();
+
+			Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+			roles.add(adminRole);
+
+			user.setRoles(roles);
+			userRepository.save(user);
+
+			return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+		}
+
+		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	}
+}
