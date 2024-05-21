db81a996afa3f050ee4dfcc1b43ca12d4014f0cd
Carlos Guzman <carlosaguzman@gmail.com>
2024-05-20 17:30:53 -0700

    - implemented .env file

--- src/main/java/com/bezkoder/spring/security/postgresql/SpringBootSecurityPostgresqlApplication.java
+++ src/main/java/com/bezkoder/spring/security/postgresql/SpringBootSecurityPostgresqlApplication.java
@@ -1,7 +1,9 @@
 package com.bezkoder.spring.security.postgresql;
 
+import io.sentry.spring.boot.SentryAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
